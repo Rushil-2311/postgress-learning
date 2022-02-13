a348df09f87c16bcf3ce3ac98162c59f2a3e63e1
Rushil-2311 <rushilpatellinux2gmail.com>
2022-02-13 22:48:38 +0530

    day1/apis

--- queries.js
+++ queries.js
@@ -50,8 +50,85 @@ const createCountry = (request, response) => {
   );
 };
 
-// CREATE ROUTE FUNCTION TO UPDATE EXISTING DATABASE RECORDS
+//create a user for application
+const createUser = (req, res) => {
+  const { name, username, email, phonenumber, user_avtar } = req.body;
+  pool.query(
+    "INSERT INTO userdata(name,email,phonenumber,user_avtar,username) VALUES ($1,$2,$3,$4,$5)",
+    [name, email, phonenumber, user_avtar, username],
+    (error, capital) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).send(pool.query("SELECT * FROM userdata"));
+      }
+    }
+  );
+};
 
+//create category
+const createCategory = (req, res) => {
+  const { title } = req.body;
+  const { id } = req.params;
+  pool.query(
+    "INSERT INTO category(title,userId) VALUES ($1,$2)",
+    [title, id],
+    (error, capital) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).send("newcategory created successfully");
+      }
+    }
+  );
+};
+
+//get category with it users information
+const getCategoryById = (req, res) => {
+  const { id } = req.params;
+  pool.query(
+    "SELECT * FROM category LEFT JOIN userdata ON category.userId=userdata.userId WHERE categoryId=$1",
+    [id],
+    (error, result) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).json({ message: result.rows });
+      }
+    }
+  );
+};
+
+const createBlog = (req, res) => {
+  const { id } = req.params;
+  const { title, description, img_url, categoryId, userId } = req.body;
+  pool.query(
+    "INSERT INTO blog(title,description,img_url,categoryId,userId) VALUES($1,$2,$3,$4,$5)",
+    [title, description, img_url, categoryId, id],
+    (error, capital) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).json({ message: "blog posted successfully" });
+      }
+    }
+  );
+};
+
+const getAllBlog = (req, res) => {
+  pool.query(
+    "select * FROM blog INNER JOIN userdata ON blog.userId=userdata.userId",
+    (error, result) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).json(result.rows);
+      }
+    }
+  );
+};
+
+// CREATE ROUTE FUNCTION TO UPDATE EXISTING DATABASE RECORDS
 const updateCountry = (request, response) => {
   const id = parseInt(request.params.id);
   const { name, capital } = request.body;
@@ -87,4 +164,9 @@ module.exports = {
   createCountry,
   updateCountry,
   deleteCountry,
+  createUser,
+  createCategory,
+  getCategoryById,
+  createBlog,
+  getAllBlog,
 };
